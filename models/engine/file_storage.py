274d973fc3d677bfedf945b046c810a9ebbdb7df
3lackrukh <nathan.rhys@atlasschool.com>
2024-05-31 21:13:05 +0000

    added colon line 69

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,6 +66,6 @@ class FileStorage:
     def reload(self):
         if os.path.exists(self.file_path):
             with open(self.file_path, "r") as f:
-                for i in json.load(f)
+                for i in json.load(f):
                     self.new(BaseModel(**i))
 
