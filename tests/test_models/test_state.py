3c2d073dfa99acb6cf33db00d01fb2a81d2bf9b0
AshleyRamer <ashleyramer7@gmail.com>
2024-06-01 23:54:01 +0000

    test for state needs work..

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -3,21 +3,6 @@ from models.state import State
 
 class TestState(unittest.TestCase):
 
-    def setUp(self):
-        """Set up a common State instance for each test."""
-        self.state = State()
-
-    def test_init(self):
-        """Test State initialization."""
-        self.assertIsInstance(self.state, State)
-        self.assertEqual(self.state.name, "")
-
-    def test_inheritance_from_BaseModel(self):
-        """Test inheritance from BaseModel."""
-        self.assertIsInstance(self.state, BaseModel)
-        self.assertTrue(hasattr(self.state, "id"))
-        self.assertTrue(hasattr(self.state, "created_at"))
-        self.assertTrue(hasattr(self.state, "updated_at"))
 
 if __name__ == "__main__":
     unittest.main()
