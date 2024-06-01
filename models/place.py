2d2049384a8c3194d13da4eb81b349164f76fedc
AshleyRamer <ashleyramer7@gmail.com>
2024-06-01 22:56:28 +0000

    amenity update

--- models/place.py
+++ models/place.py
@@ -9,7 +9,10 @@ class Place(BaseModel):
     city_id = ""
     name = ""
     description = ""
-    price = 0
-    number_of_rooms = 0
-    number_of_bathrooms = 0
-    amenities = []
+    price_by_night = 0
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
