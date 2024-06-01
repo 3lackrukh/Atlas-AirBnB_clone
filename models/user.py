abd50853317ec1389631e0d34b45addc8a45c9e9
AshleyRamer <ashleyramer7@gmail.com>
2024-06-01 22:53:09 +0000

    task 8

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class"""
-    user_email = ""
-    user_password = ""
+    email = ""
+    password = ""
     first_name = ""
     last_name = ""
