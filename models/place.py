201c8596ce7abda27440ed8dc6de70771d956614
AshleyRamer <ashleyramer7@gmail.com>
2024-06-01 22:07:13 +0000

    rough draftfor Place subclass

---
+++ models/place.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Place Subclass"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Place class"""
+    user_id = ""
+    city_id = ""
+    name = ""
+    description = ""
+    price = 0
+    number_of_rooms = 0
+    number_of_bathrooms = 0
+    amenities = []
