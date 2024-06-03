f6bd910635dab0d827fb6bd5962f1ff6685c4f7f
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-03 04:17:17 +0000

    removed unnecessary variable declaration 'data' line 88

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -85,7 +85,5 @@ class FileStorage:
     def reload(self):
         if os.path.exists(self.file_path):
             with open(self.file_path, "r") as f:
-                data = json.load(f)
-                for k, v in data.items():
+                for k, v in json.load(f).items():
                     self.new(classes[v.get('__class__')](**v))
-                    #self.new(obj)
