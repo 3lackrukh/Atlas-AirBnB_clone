fe808dc682a83a2310d8f511cc479418e0f8a05d
AshleyRamer <ashleyramer7@gmail.com>
2024-06-03 17:12:13 +0000

    added init

--- models/state.py
+++ models/state.py
@@ -5,14 +5,5 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """State class"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._name = ""
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        self._name = value
+    def __init__(self):
+        self.name = ""
