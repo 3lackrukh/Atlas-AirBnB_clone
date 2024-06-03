161bf2bdd4cd22bce2f3b67d94d94252aaf12533
AshleyRamer <ashleyramer7@gmail.com>
2024-06-03 17:47:15 +0000

    fixed test for place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -13,4 +13,16 @@ from models.user import User
 class Test_Place(unittest.TestCase):
     """tests for the Place class"""
     def test_1(self):
-        pass
+        obj = State()
+        assertIsInstance(obj.name, str)
+        assertIsInstance(obj.user_id, str)
+        assertIsInstance(obj.city_id, str)
+        assertIsInstance(obj.description, str)
+        assertIsInstance(obj.price_by_night, int)
+        assertIsInstance(obj.number_rooms, int)
+        assertIsInstance(obj.number_bathrooms, int)
+        assertIsInstance(obj.max_guest, int)
+        assertIsInstance(obj.longitude, int)
+        assertIsInstance(obj.latitude, int)
+        assertIsInstance(obj.amenity_ids, list)
+        
