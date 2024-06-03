2c5fa7bd6d5db0d2ad3d2b47bd29c3fff65cd33e
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-03 01:36:44 +0000

    ensures id is a string object lin 42

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,7 @@ class BaseModel:
 
         else:
             # New instance
-            self.id = uuid.uuid4().hex
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             # Add new instance to storage
