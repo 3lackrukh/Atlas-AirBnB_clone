70879b6add1313c9416829b0260489d9a7467bf7
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-03 17:50:10 +0000

    new tests for FileStorage

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 """Tests for file storage"""
 import unittest
 import models
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
@@ -16,13 +17,27 @@ from models.engine.file_storage import FileStorage
 
 class MyFileStorage(unittest.TestCase):
     
+    def setUp(self):
+        """set up for testing"""
+        self.file_path = "file_test.json"
+        self.storage = FileStorage()
+        self.storage.__file_path = self.file_path
+        self.obj = BaseModel()
+    
+    def tearDown(self):
+        """memory cleanup after testing"""
+        try:
+            os.remove(self.file_path)
+        except FileNotFoundError:
+            pass
+
     def test_file_path(self):
         """Tests __file_path exists and is type str"""
-        self.assertIsInstance(storage.__file_path, str)
+        self.assertIsInstance(self.storage.__file_path, str)
 
     def test_objects(self):
         """Tests __objects exists and is type dict"""
-        self.assertIsInstance(storage.__objects, dict)
+        self.assertIsInstance(self.storage.__objects, dict)
 
     def test_new(self):
         """Tests to make sure objects are stored as dictionary objects"""
