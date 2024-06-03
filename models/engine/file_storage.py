a2a4010907039aca5d2ecc89093241d62da560d5
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-03 17:30:34 +0000

    added try block to reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,8 +65,11 @@ class FileStorage:
             json.dump(obj_dict, f)
 
     def reload(self):
-        if os.path.exists(self.__file_path):
+        #if os.path.exists(self.__file_path):
+        try:
             with open(self.__file_path, "r", encoding="utf-8") as f:
                 for k, v in json.load(f).items():
                     obj = (classes[v.get('__class__')](**v))
                     self.__objects[k] = obj
+        except FileNotFoundError:
+            pass
