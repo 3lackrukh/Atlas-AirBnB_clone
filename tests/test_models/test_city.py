b8df60286e1244c86eb192a566849269e5cba4d1
AshleyRamer <ashleyramer7@gmail.com>
2024-06-03 16:26:54 +0000

    updated city test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -14,15 +14,3 @@ class Test_City(unittest.TestCase):
     """tests for the City class"""
     def test_1(self):
         pass
-    
-    def test_2(self):
-        pass
-
-    def test_3(self):
-        pass
-
-    def test_4(self):
-        pass
-
-    def test_5(self):
-        pass
