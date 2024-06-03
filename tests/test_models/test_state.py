a17bb711388df40129abda05c0c6e559dbff12f9
AshleyRamer <ashleyramer7@gmail.com>
2024-06-03 17:13:03 +0000

    changed state test file

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -14,15 +14,3 @@ class Test_State(unittest.TestCase):
     """tests for the State class"""
     def test_1(self):
         pass
-    
-    def test_2(self):
-        pass
-
-    def test_3(self):
-        pass
-
-    def test_4(self):
-        pass
-
-    def test_5(self):
-        pass
