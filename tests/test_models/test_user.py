e1938b642931558ec0d10e74371d5d259e978885
AshleyRamer <ashleyramer7@gmail.com>
2024-06-04 16:34:48 +0000

    fixed test for user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -14,4 +14,9 @@ from models.user import User
 class Test_User(unittest.TestCase):
     """tests for the User class"""
     def test_1(self):
-        pass
+        obj = User()
+        self.assertIsInstance(obj.email, str)
+        self.assertIsInstance(obj.password, str)
+        self.assertIsInstance(obj.first_name, str)
+        self.assertIsInstance(obj.last_name, str)
+        
