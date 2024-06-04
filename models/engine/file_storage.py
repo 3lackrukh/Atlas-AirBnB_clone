aa57e378a0c5943397c31fb5b39eccfc43a8d582
AshleyRamer <ashleyramer7@gmail.com>
2024-06-04 16:48:07 +0000

    fixed whitespace in ln 89 file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -86,4 +86,4 @@ class FileStorage():
         if os.path.exists(self.__file_path):
             with open(self.__file_path, "r", encoding="utf-8") as f:
                 for k, v in json.load(f).items():
-                    self.new(classes[v.get('__class__')](**v))        
+                    self.new(classes[v.get('__class__')](**v))
